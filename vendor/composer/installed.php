2bf2e4e78b8f65dacf5c4f5cd739cc55fed01c54
Philia093-Cyrene <furina520@foxmail.com>
2026-02-11 13:43:13 +0800

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'myapp/app',
+  ),
+  'versions' => 
+  array (
+    'myapp/app' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'overtrue/chinese-calendar' => 
+    array (
+      'pretty_version' => '1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd78e86d66a4b225defac6828bf1de42c4a9d4bdf',
+    ),
+  ),
+);
